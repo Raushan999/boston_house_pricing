8900d83e5c84c825b7a02583b096e27b75575e92
Raushan999 <rprakash1806@kgpian.iitkgp.ac.in>
2023-05-22 17:33:19 +0530

    the web application

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
+import json
 import pickle
-from flask import Flask, request, app, jsonify,url_for, render_template
+
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 #starting point of the application: from where the app will run
-app = Flask(__name__)
+app=Flask(__name__)
 
 #loading the model
-regmodel = pickle.load(open('regmodel.pkl', 'rb'))
-scalar = pickle.load(open('scaling.pkl', 'rb'))
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 
 #defining the route for the application
 @app.route('/')
@@ -37,32 +39,6 @@ def predict():
     print(final_input)
     output=regmodel.predict(final_input)[0]
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
-
 # running the app
-if __name__ == '__main__':
-    app.run(debug=True)
-    
-# import json
-# import pickle
-
-# from flask import Flask,request,app,jsonify,url_for,render_template
-# import numpy as np
-# import pandas as pd
-
-# app=Flask(__name__)
-# ## Load the model
-# regmodel=pickle.load(open('regmodel.pkl','rb'))
-# scalar=pickle.load(open('scaling.pkl','rb'))
-# @app.route('/')
-# def home():
-#     return render_template('home.html')
-
-# @app.route('/predict_api',methods=['POST'])
-# def predict_api():
-#     data=request.json['data']
-#     print(data)
-#     print(np.array(list(data.values())).reshape(1,-1))
-#     new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-#     output=regmodel.predict(new_data)
-#     print(output[0])
-#     return jsonify(output[0])
+if __name__=="__main__":
+    app.run(debug=True)  
